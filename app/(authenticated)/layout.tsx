9fc0599bae71b0a1e3c82c76c0ac45a7bfee8c11
ydideh810 <yd.baller810@gmail.com>
2024-10-24 21:22:07 -0400

    Update layout.tsx

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -3,6 +3,8 @@ import { redirect } from 'next/navigation';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
+export const dynamic = 'force-dynamic';
+
 export default async function AuthenticatedLayout({
   children,
 }: {
