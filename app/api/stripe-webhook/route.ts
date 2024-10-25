47b150cdf464bba745ebbeae274d618d4fc7a9d5
ydideh810 <yd.baller810@gmail.com>
2024-10-24 21:17:21 -0400

    Update route.ts

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -8,7 +8,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16',
 });
 
-export const runtime = 'edge';
 export const dynamic = 'force-dynamic';
 
 export async function POST(req: Request) {
